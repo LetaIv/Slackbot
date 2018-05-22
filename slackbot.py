2b5c44fd4766a4a222e04d30ccc29faf545eba37
Ivan Letal <il@oc1881171831.ibm.com>
2018-05-22 21:03:11 +0200

    Initial version

---
+++ slackbot.py
@@ -0,0 +1,97 @@
+# Slack bot
+# Author: Ivan Letal
+# Licence: IBM intellectual property
+# Date: 2018/04/19
+# Before you use the script:	export SLACK_BOT_TOKEN='<your slack bot token>'
+
+
+import xforce_addon as addon # you can code your own and replace this
+						 # addon must contain functions: do_event_mention(), do_event_async(), do_event_joinchannel(), do_event_message()
+from slackclient import SlackClient # Slack API
+import re
+import os
+import time
+from datetime import datetime
+
+
+channels = [] # channels the bot is in, used for listening to channels
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+starterbot_id = None # value is assigned after the bot starts up
+RTM_READ_DELAY = 1 # number of second delay between reading from RTM
+MENTION_REGEX = '^<@(|[WU].+?)>(.*)'
+VERBOSE = False
+
+def parse_events(slack_events): # parses a list of events coming from the Slack RTM API
+  for event in slack_events:
+    if event['type'] == 'message' and not 'subtype' in event:   
+      global channels
+      channel = event['channel']
+      if not channel in channels:
+        channels.append(channel)
+        do_event_joinchannel(channel)
+      user_id, command = test_if_mentioned(event['text'])
+      if user_id == starterbot_id: # bot is mentioned
+        return 'mention', command, event['channel']
+      if starterbot_id <> event['user']: # bot is not mentioned, some message arrived in channel (from other user than bot itself)
+        return 'message', command, event['channel']
+  return None, None, None
+
+def test_if_mentioned(msg_text): # tests for a direct mention (@) in message text and returns the user id which was mentioned
+  matches = re.search(MENTION_REGEX, msg_text)
+  return (matches.group(1), matches.group(2).strip()) if matches else (None, None) # the first group contains the username, the second group contains the remaining message
+
+def log_time(): # return date / time
+  return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def get_channel_name(channel_id): # return channel name
+  channel_info = slack_client.api_call('channels.info', channel=channel_id)
+  if 'channel' in channel_info: return channel_info['channel']['name']
+  else: return channel_id # unable to get channel name, e.g. direct message was received, return ID
+
+def do_event_mention(channel, command): # run event when bot is mentioned, execute command
+  print '{} {} Bot was mentioned.'.format(log_time(), channel)
+  default_response = 'Internal error, addon did not provide any answer.' # default response is help text for the user
+  parts = command.split(' ') # syntax: command_text some_text
+  part1 = parts[0]
+  part2 = ' '.join(parts[1:])
+  response = addon.do_event_mention(channel, part1, part2)
+  slack_client.api_call('chat.postMessage', channel=channel, text=response or default_response, as_user=True)
+
+def do_event_message(channel, command): # run event when a new message is posted to a channel
+  if VERBOSE: print '{} {} Someone wrote to the channel.'.format(log_time(), channel)
+  response = 'Spam, sorry.'
+  # not implemented
+  # slack_client.api_call('chat.postMessage', channel=channel, text=response, as_user=True)
+
+def do_event_async(channel): # run event for every channel periodically, usually every 1 second
+  if VERBOSE: print '{} {} Asynchronous event.'.format(log_time(), channel)
+  message = addon.do_event_async(channel)
+  if message <> None: slack_client.api_call('chat.postMessage', channel=channel, text=message, as_user=True)
+
+def do_event_joinchannel(channel): # run event when bot joins a new channel
+  print '{} {} Slackbot joined channel.'.format(log_time(), channel)
+  print '{} Current channel list: {}.'.format(log_time(), str(channels))
+  message = addon.do_event_joinchannel(channel, get_channel_name(channel))
+  if message <> None: slack_client.api_call('chat.postMessage', channel=channel, text='{}'.format(message), as_user=True)
+
+
+if __name__ == '__main__':
+  if slack_client.rtm_connect(with_team_state = False):
+    while True: # keep reconnecting
+      #try:
+        starterbot_id = slack_client.api_call('auth.test')['user_id'] # read bot's user id by calling web api method 'auth.test'
+        print '{} Slackbot connected and running.'.format(log_time())
+        while True:            
+          for ch in channels: do_event_async(ch)
+          msg_type, command, channel = parse_events(slack_client.rtm_read())            
+          if msg_type == 'mention': 
+            if command: do_event_mention(channel, command)
+          elif msg_type == 'message': 
+            if command: do_event_message(channel, command)
+          time.sleep(RTM_READ_DELAY)
+     # except:
+     #   print '{} Lost connection to Slack, reconnecting in 10 seconds.'.format(log_time())
+     #   time.sleep(10)
+     #   if slack_client.rtm_connect(with_team_state = False): pass
+  else:
+    print '{} Error connecting to Slack.'.format(log_time())
---
+++ xforce_addon.py
@@ -0,0 +1,208 @@
+# X-Force addon for Slack bot
+# Author: Ivan Letal
+# Licence: IBM intellectual property
+# Date: 2018/04/19
+
+
+import xforce_api
+import json
+import os
+from datetime import datetime, timedelta
+
+
+Monitor_list = [{'channel_id' : '123456789', 'text' : 'what to monitor', 'last' : 0}] # first example item to note syntax, should be ingored in further implementations
+Channels = {} # {'channel_id' : {'channel_name' : string_name, 'sched_every' : int_minutes, 'sched_next' : datetime_now}}
+MAX_RESULTS= 5 # search for last X vulnerabilities only
+
+######### (OPTIONAL) INTERNAL DEFINITIONS ###################
+
+def commands():
+  return ['list', 'test', 'add', 'remove', 'run', 'restore', 'save', 'schedule']
+
+def help_text():
+  return 'Unknown command, available commands are: {}'.format(str(commands()))
+
+def command_test(arg): # return maximum of N searches, short output
+  json_obj = xforce_api.search(arg)
+  if 'error' in json_obj: return 'No results found.'
+  response = ''
+  count = 0
+  for row in json_obj['rows']:
+    count += 1
+    response += '*#{}* '.format(str(count)) + '\n'  
+    response += '*Title:* ' + row['title'] + '\n'
+    response += '*Reported:* {}'.format(row['reported']) + '\n'
+    response += '*ID:* ' + str(row['xfdbid']) + '\n'
+    response += '*Risk level:* ' + str(row['risk_level']) + '\n'
+    if 'platforms_affected' in row: response += '*Platforms affected:* ' + ', '.join(row['platforms_affected']) + '\n'
+    response += '*References:* ' + '\n'
+    for reference in row['references']:
+      response += reference['link_name'] + '\n' + reference['link_target'] + '\n'
+      break # list only one reference
+    response += '\n'
+    if count == MAX_RESULTS: break
+  return response
+
+def command_add(channel, arg): # adds text to monitor list for this channel
+  global Monitor_list
+  Monitor_list.append({'channel_id' : channel, 'text' : arg, 'last' : 0})
+  return 'Successfully added.'
+
+def command_remove(channel, arg): # remote text from monitor list for this channel
+  global Monitor_list
+  new_list = []
+  for i in Monitor_list:
+    if not (i['channel_id'] == channel and i['text'] == arg):
+      new_list.append(i)
+  if Monitor_list == new_list: 
+    return 'Nothing was removed.'
+  else:
+    Monitor_list = new_list
+    return 'Successfully removed.'
+
+def command_list(channel): # list monitor list
+  response = ''
+  count = 0
+  for x in Monitor_list:
+    if x['channel_id'] == channel:
+      count += 1
+      response += '#{}: {}'.format(str(count), x['text']) + '\n'
+  if count == 0: return 'Nothing is monitored for this channel.'
+  return response
+
+def command_run(channel, arg): # runs report now
+  if not arg == 'now': return 'Use "run now" instead.'
+  global Monitor_list
+  response = ''
+  for idx, val in enumerate(Monitor_list):
+    x = Monitor_list[idx]
+    if x['channel_id'] == channel:
+      last = int(x['last'])
+      text = x['text']
+      json_obj = xforce_api.search(text)
+      if 'error' in json_obj: return 'Internal error.'
+      last_new = last
+      for row in json_obj['rows']:
+        if int(row['xfdbid']) > last:
+          # report item	  
+          if int(row['xfdbid']) > last_new: 
+            last_new = int(row['xfdbid']) # save the new last / highest ID          
+            Monitor_list[idx]['last'] = last_new
+          response += '*Title:* ' + row['title'] + '\n'
+          response += '*Reported:* {}'.format(row['reported']) + '\n'
+          response += '*ID:* ' + str(row['xfdbid']) + '\n'
+          response += '*Risk level:* ' + str(row['risk_level']) + '\n'
+          if 'platforms_affected' in row:  response += '*Platforms affected:* ' + ', '.join(row['platforms_affected']) + '\n'
+          response += '*References:* ' + '\n'
+          for reference in row['references']:
+            response += reference['link_name'] + '\n' + reference['link_target'] + '\n'
+            break # list only one reference
+          response += '\n'                    
+  if not response: return 'No news.'
+  else: 
+    command_save(channel):
+    return response
+
+def command_save(channel):
+  my_file = './saved_list_' + channel + '.txt'
+  with open(my_file, 'w') as f: 
+    for x in Monitor_list:
+      if x['channel_id'] == channel:
+        f.write(x['text'] + ';' + str(x['last']) + '\n')
+    f.close()
+    return 'List saved successfully.'
+  return 'Error while saving list.'
+
+def command_restore(channel):
+  my_file = './saved_list_' + channel + '.txt'
+  if os.path.exists(my_file): 
+    with open(my_file, 'r') as f: 
+      for line in f.readlines():
+        text, last = line.split(';')
+        x = {'channel_id' : channel, 'text' : text, 'last' : int(last)}
+        if not x in Monitor_list: Monitor_list.append(x)      
+  else: return 'Error while loading list.'
+  return 'List restored successfully.'
+
+def command_schedule(channel, arg):
+  global Channels
+  try:
+    x = int(arg)
+    x += 1
+  except:
+    if not arg == 'disable':
+      return 'Invalid argument. It can be "disable" or integer value in range 1-99999.'
+    else: 
+      Channels[channel]['sched_every'] = 0
+      print '{} ADDON: Schedule for channel "{}" was disabled.'.format(log_time(), Channels[channel]['channel_name'])
+      return 'Schedule was disabled.'
+  if 1 <= x <= 99999:
+    Channels[channel]['sched_every'] = x
+    Channels[channel]['sched_next'] = datetime.now().replace(second=0, microsecond=0) + timedelta(minutes=Channels[channel]['sched_every'])
+    print '{} ADDON: New schedule for channel "{}" was set to {}.'.format(log_time(), Channels[channel]['channel_name'], Channels[channel]['sched_next'])
+    return 'Schedule was enabled. Next run is at {}.'.format(Channels[channel]['sched_next'])
+  else:
+    return 'Invalid argument. It can be "disable" or integer value in range 1-99999.'
+
+def log_time(): # return date / time
+  return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+######### (MANDATORY) EXTERNAL ADDON DEFINITIONS ######### 
+
+def do_event_mention(channel, part1, part2):
+  if part1 not in commands(): return 'Unknown command. Use: {}'.format(commands())
+  if part1 == 'list': return command_list(channel)
+  elif part1 == 'test' and part2: return command_test(part2)
+  elif part1 == 'add' and part2: return command_add(channel, part2)
+  elif part1 == 'remove' and part2: return command_remove(channel, part2)
+  elif part1 == 'run' and part2: return command_run(channel, part2)
+  elif part1 == 'save': return command_save(channel)
+  elif part1 == 'restore': return command_restore(channel)
+  elif part1 == 'schedule' and part2: return command_schedule(channel, part2)
+  else: return 'Missing argument.'
+
+def do_event_async(channel):
+  global Channels
+  if Channels[channel]['sched_every'] > 0:    
+    today = datetime.now().replace(microsecond=0)   
+    if today > Channels[channel]['sched_next']:  
+      Channels[channel]['sched_next'] = today + timedelta(minutes=Channels[channel]['sched_every'])
+      print '{} ADDON: New schedule for channel "{}" was set to {}.'.format(log_time(), Channels[channel]['channel_name'], Channels[channel]['sched_next'])
+      return command_run(channel, 'now')
+  return None # if nothing is scheduled or schedule already run
+
+def do_event_joinchannel(channel_id, channel_name):
+  global Channels
+  sched_every = 0
+  sched_next = datetime.now().replace(second=0, microsecond=0)
+  Channels[channel_id] = {'channel_name' : channel_name, 'sched_every' : sched_every, 'sched_next' : sched_next}
+  print '{} ADDON: Channel name is "{}".'.format(log_time(), Channels[channel_id]['channel_name'])
+  return None
+  
+
+#############################################################
+
+ 	  
+if __name__ == '__main__':
+  # 1)
+  # json_obj = xforce_api.search('airwatch')
+  # print json.dumps(json_obj, indent = 4, sort_keys = True)
+  # print '*****************************************************'
+
+  # 2)
+  # print do_event_mention(None, 'test', 'airwatch')
+
+  #3)
+  # print command_test('airwatch')
+ 
+  # 4)
+  print command_add('CB51N66S1', 'airwatch')
+  print command_add('CB51N66S1', 'Blue Coat ProxySG')
+  # print command_remove('CB51N66S1', 'Blue Coat ProxySG')
+  print command_add('wrongID', 'does not matter')
+  print command_list('CB51N66S1')
+
+  # 5)
+  print command_run('CB51N66S1', 'now')
+  print command_run('CB51N66S1', 'now') # will not do anything
